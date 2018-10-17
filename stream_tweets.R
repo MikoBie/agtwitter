77350822e0be2ee7ba249be099e43785c4c8c71a
MikoBie <profesor_floretu@op.pl>
2018-10-17 16:24:18 +0200

    Dodanie skryptu do ściągania tweetów, z wbudowanym mechnizmem do zatrzymywania ściągania

---
+++ stream_tweets.R
@@ -0,0 +1,53 @@
+#!/usr/bin/Rscript --vanilla
+
+cat('\nLoad libraries')
+list.of.packages <- c("rtweet",
+                      "tidyverse",
+                      "magrittr",
+                      "dplyr",
+                      "openssl",
+                      "stringr",
+                      "jsonlite")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+
+cat('\nSet variables')
+ROOT_PATH <- getwd()
+DATA_PATH <- file.path(ROOT_PATH,"data")
+WORDS <- c("kler",
+           "KLER",
+           "ksiądz",
+           "księża",
+           "ks.",
+           "ks",
+           "biskup",
+           "arcybiskup",
+           "smarzewski",
+           "Smarzewski",
+           "gajos",
+           "Gajos",
+           "Wieckiewicz",
+           "Wieckiewicz",
+           "wieckiewicz",
+           "więckiewicz",
+           "kościół",
+           "kosciol",
+           "ksiadz",
+           "ksieza",
+           "pedofilia",
+           "pedofil") %>%
+  paste(collapse = ", ")
+
+con_out <- file(file.path(DATA_PATH,"stream.jl"), open="wb")
+  while(readLines("control.txt") == "CONTINUE"){
+    stream_tweets(q = WORDS,
+                  timeout = 60 * 90,
+                  parse = TRUE) %>%
+      filter(lang == "pl") %>%
+      stream_out(x = .,
+                 con = con_out,
+                 pagesize = 1)
+  }
+close(con_out)
