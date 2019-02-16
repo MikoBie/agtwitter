e869111007eba6947d453f89656edda53deb3685
MikoBie <profesor_floretu@op.pl>
2019-02-16 22:47:26 +0100

    Afera taśmowa - fix_connection.R

---
+++ fix_reconnection.R
@@ -0,0 +1,49 @@
+#!/usr/bin/Rscript --vanilla
+
+cat('\nLoad libraries')
+list.of.packages <- c("rtweet",
+                      "tidyverse",
+                      "magrittr",
+                      "jsonlite",
+                      "stringr")
+
+
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+
+cat('\nType in the name of the file and hit enter:')
+
+file_name <- file.info(dir()) %>%
+  mutate(name = rownames(.)) %>%
+  filter(size > 0) %>%
+  filter(str_detect(name,".json")) %$%
+  name
+
+file_name_out <- file_name %>%
+  str_remove("-") %>%
+  str_split("(?<=([:lower:]{6}\\d{4}))") %>%
+  unlist() %>%
+  paste(collapse = "_") %>%
+  str_split("(?<=([_]\\d{2}))") %>%
+  unlist() %>%
+  paste(collapse = "_") %>%
+  str_split("(?<=([_]\\d{2}))") %>%
+  unlist() %>%
+  paste(collapse = "_") %>%
+  str_split("(?<=([_]\\d{2}))") %>%
+  unlist() %>%
+  paste(collapse = "_") %>%
+  str_split("(?<=([_]\\d{2}))") %>%
+  unlist() %>%
+  paste(collapse = "_") %>%
+  str_replace_all("[_]{2,}", "_") %>%
+  str_replace("json", "jl")
+  
+  
+cat('/nLoad data')
+parse_stream(file_name) %>%
+  filter(lang == "pl") %>%
+  stream_out(file(file_name_out))
+
